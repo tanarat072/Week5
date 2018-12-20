90ebd2e269501c24ba36b0106e6f94261d2d9f16
jarudat chaikuad <60030014@kmitl.ac.th>
2018-12-20 19:32:45 +0700

    1

---
+++ Week5/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Week5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using System;
+public class GotoTest
+        {
+            static void Main(string[] args)
+            {
+                Console.WriteLine("Line 1");
+                Console.WriteLine("Line 2");
+                Console.WriteLine("Line 3");
+            line4:
+                Console.WriteLine("Line 4");
+                Console.WriteLine("Line 5");
+                Console.WriteLine("Line 6");
+                goto line10;
+                Console.WriteLine("Line 7");
+                Console.WriteLine("Line 8");
+                Console.WriteLine("Line 9");
+            line10:
+                Console.WriteLine("Line 10");
+            }
+        }
+
+    }
+}
+}
